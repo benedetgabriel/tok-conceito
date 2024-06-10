22a78592b569d616cb8f4bbc6f25e59d76e018a8
Gabriel Benedet <gabrielrodrigues@pecege.com>
2024-06-10 14:38:14 -0300

    fix: resizing images

--- src/components/HomeNew.jsx
+++ src/components/HomeNew.jsx
@@ -17,21 +17,20 @@ import HomeNewMobile from './mobile/HomeNewMobile';
 const HomeNew = () => {
   const [isMobile, setIsMobile] = useState(false);
   const isMobileQuery = useMediaQuery({ query: `(max-width: 767px)` });
-  const [images] = useState([Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10]);
+  const [images, setImages] = useState([Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10]);
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   const smoothScroll = (sectionId, offset = 70) => {
     let sectionElement = document.querySelector(sectionId);
     sectionElement.scrollIntoView({
-      behavior: 'smooth'
+        behavior: 'smooth'
     });
     const yCoordinate = sectionElement.getBoundingClientRect().top + window.pageYOffset;
     const yOffset = -offset; 
     window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
-  };
+};
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -39,11 +38,16 @@ const HomeNew = () => {
     }, 4000);
     return () => clearInterval(timer);
   }, [images]);
-
+  
   useEffect(() => {
     const onScroll = () => {
       const scrollPos = window.scrollY;
-      setScrolled(scrollPos > 0);
+
+      if (scrollPos > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
 
     window.addEventListener("scroll", onScroll);
@@ -57,51 +61,25 @@ const HomeNew = () => {
     setIsMobile(isMobileQuery);
   }, [isMobileQuery]);
 
-  useEffect(() => {
-    const preloadImages = async () => {
-      const promises = images.map((src) => {
-        return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = src;
-          img.onload = resolve;
-          img.onerror = reject;
-        });
-      });
-
-      try {
-        await Promise.all(promises);
-        setImagesLoaded(true);
-      } catch (error) {
-        console.error('Erro ao carregar imagens:', error);
-      }
-    };
-
-    preloadImages();
-  }, [images]);
-
-  if (!imagesLoaded) {
-    return <div className={styles.loading}>Carregando...</div>; // Substitua pelo seu componente de carregamento
-  }
-
   if (isMobile) {
     return <HomeNewMobile />;
   }
 
-  return (
+  return(
     <>
       <header className={`${styles.header} ${scrolled ? styles.headerScrolled : ''}`}>
         <nav className={styles.nav}>
-          <img src={Logo} alt="Logo" />
+          <img src={Logo} />
           <ul className={styles.links}>
-            <li><a style={{ cursor: 'pointer' }} onClick={() => smoothScroll("#home", 130)}>Home</a></li>
-            <li><a style={{ cursor: 'pointer' }} onClick={() => smoothScroll("#product", 130)}>Produtos</a></li>
-            <li><a style={{ cursor: 'pointer' }} onClick={() => smoothScroll("#review", 130)}>Depoimentos</a></li>
-            <li><a style={{ cursor: 'pointer' }} onClick={() => smoothScroll("#about", 130)}>Sobre nós</a></li>
-            <li><a style={{ cursor: 'pointer' }} onClick={() => smoothScroll("#contact", 130)}>Contato</a></li>
+            <li><a style={{cursor: 'pointer'}} onClick={() => smoothScroll("#home", 130)}>Home</a></li>
+            <li><a style={{cursor: 'pointer'}} onClick={() => smoothScroll("#product", 130)}>Produtos</a></li>
+            <li><a style={{cursor: 'pointer'}} onClick={() => smoothScroll("#review", 130)}>Depoimentos</a></li>
+            <li><a style={{cursor: 'pointer'}} onClick={() => smoothScroll("#about", 130)}>Sobre nós</a></li>
+            <li><a style={{cursor: 'pointer'}} onClick={() => smoothScroll("#contact", 130)}>Contato</a></li>
           </ul> 
           <button className={styles.contact} onClick={() => smoothScroll("#contact")}>Fale conosco</button>
-        </nav>
-      </header>
+        </nav> 
+      </header> 
 
       <section className={`${styles.home} ${fade ? 'fadeIn' : 'fadeOut'}`} id="home">
         <div className={styles.imageWrapper}>
@@ -116,10 +94,11 @@ const HomeNew = () => {
             <p>Na nossa loja, você encontrará produtos de alta qualidade e um atendimento personalizado para ajudá-lo a encontrar a solução perfeita para a sua decoração.</p>
           </div>
         </div>
+
+        <div className={styles.details_bottom}></div>
       </section>
-      <div className={styles.details_bottom}></div>
     </>
-  );
+  )
 }
 
-export default HomeNew;
+export default HomeNew
